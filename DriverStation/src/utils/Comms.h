a4f999d1bc9d78061025c48f5e0be69cb378be14
Zonglin-Li6565 <lizonglin6565@163.com>
2017-01-30 20:18:41 -0600

    PC side communication

--- DriverStation/src/utils/Comms.h
+++ DriverStation/src/utils/Comms.h
@@ -1,27 +1,41 @@
 #pragma once
 
-#define BAUD_RATE	115200
+#define BAUD_RATE	9600
 #define TIMEOUT		500
 
 #include "../robot/RobotIO.h"
+#include "CRC8.h"
 #include <serial/serial.h>
 #include <iostream>
+#include <windows.h>
 using namespace serial;
 
 class Comms {
 public:
+    // Public functions should not call each other. Otherwise deadlock
 	Comms();
 
-	bool read();
-	bool write();
+    bool read();
+    bool write();
 
-	bool maintainConnection();
+    void begin();
+    void end();
 
-	RobotIn in;
-	RobotOut out;
+    RobotIn getRobotIn();
+    RobotOut getRobotOut();
+    void setRobotOut(const RobotOut &newStruct);
 
+    bool checkserial();
+    bool isEnded();
 private:
+    // private functions should not take lock
 	Serial* serial;
-
+    HANDLE mutex;
+    HANDLE threadHandle;
+    RobotOut out;
+    RobotIn in;
+    CRC8 crc8;
+    bool ended;
 	void enumerate_ports();
+    bool maintainConnection();
 };
