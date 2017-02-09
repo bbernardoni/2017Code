4a20189d1596f2c8b98037406f1c16e863dc8904
bbernardoni <bennett.bernardoni@gmail.com>
2017-02-09 13:13:46 -0600

    Added config for ease of changing pins

---
+++ Robot/Config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Pin mappings
+#define GYRO_PIN      53
+#define DRIVE_FL_PIN  12
+#define DRIVE_BL_PIN  11
+#define DRIVE_FR_PIN  9
+#define DRIVE_BR_PIN  10
+#define JUMP_PIN      32
+
+// Comms
+#define BAUD_RATE     57600
+
+#endif /* CONFIG_H */
