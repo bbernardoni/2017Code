dc777822335f5330ec2355b67f47a68d3cab97af
bbernardoni <bennett.bernardoni@gmail.com>
2017-02-14 21:55:59 -0600

    Added key and ball subsystems

---
+++ DriverStation/src/robot/Ball.cpp
@@ -0,0 +1,19 @@
+#include "Ball.h"
+
+Ball::Ball(){
+
+}
+
+void Ball::periodic(const RobotIn& rIn, RobotOut& rOut){
+	// check for ball intake button (create in Controls.h)
+	// and toggle intake motor
+
+
+	// check for raise door button and toggle solenoid
+
+
+	// check for score motor and index servo motor
+	// see golf ball scorer from last years code 
+	// -- https://github.com/MRDC2019/iRobotics_Personal/blob/master/RobotMain/GolfBalls.cpp
+
+}
