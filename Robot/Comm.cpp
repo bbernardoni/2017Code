442bbd57348d773239b4af7825a3ccd85246a27a
bbernardoni <bennett.bernardoni@gmail.com>
2017-03-04 13:31:28 -0600

    Added the rest of the IO

--- Robot/Comm.cpp
+++ Robot/Comm.cpp
@@ -10,25 +10,31 @@ bool Comm::read(){
   int bytes_avail = Serial.available();
   //Serial.print("bytes_avail = ");
   //Serial.println(bytes_avail);
-  if(bytes_avail < 16){
+  if(bytes_avail < 28){
     failures++;
     return false;
   }
   
   Serial.readBytes(read_buf, bytes_avail);
-  for (int i = bytes_avail - 1; i >= 7; i --) {   // search for last complete packet
-    if (read_buf[i] == 0xdd && read_buf[i - 7] == 0xff) {   // a complete packet
-      uint8_t crc = _crc8(&read_buf[i - 6], 5);
+  for (int i = bytes_avail - 1; i >= 13; i --) {   // search for last complete packet
+    if (read_buf[i] == 0xdd && read_buf[i - 13] == 0xff) {   // a complete packet
+      uint8_t crc = _crc8(&read_buf[i - 12], 11);
       if (crc != read_buf[i - 1]) {
-        i -= 8;
+        i -= 14;
         continue;
       }
       //Serial.println("Read successful");
-      _out_struct->driveFL = read_buf[i - 6];     //TODO: potential race condition. No synchronization primitives
-      _out_struct->driveBL = read_buf[i - 5];
-      _out_struct->driveFR = read_buf[i - 4];
-      _out_struct->driveBR = read_buf[i - 3];
-      _out_struct->omni    = read_buf[i - 2];
+      _out_struct->driveFL = read_buf[i - 12];     //TODO: potential race condition. No synchronization primitives
+      _out_struct->driveBL = read_buf[i - 11];
+      _out_struct->driveFR = read_buf[i - 10];
+      _out_struct->driveBR = read_buf[i - 9];
+      _out_struct->omni    = read_buf[i - 8];
+      _out_struct->shoulder= read_buf[i - 7];
+      _out_struct->wrist   = read_buf[i - 6];
+      _out_struct->keyGrabber= read_buf[i - 5];
+      _out_struct->intake  = read_buf[i - 4];
+      _out_struct->score   = read_buf[i - 3];
+      _out_struct->door    = read_buf[i - 2];
       break;
     }
   }
@@ -47,8 +53,8 @@ void Comm::write(){
       char t = Serial.read();
     }
   }
-  Serial.write(outBuf, 8);
-  Serial.write(outBuf, 8);
+  Serial.write(outBuf, 13);
+  Serial.write(outBuf, 13);
 }
 
 int Comm::write(unsigned char * msg, int len) {
@@ -78,9 +84,14 @@ void Comm::setOutBuf(){
   outBuf[0] = 0xff;
   float *tmp = (float *)(outBuf + 1);
   *tmp = _in_struct->gyroAngle;
-  outBuf[5] = _crc8(&outBuf[1], 4);
-  outBuf[6] = 0xee;
-  outBuf[7] = 0xdd;
+  outBuf[5] = _in_struct->sonicDistanceF;
+  outBuf[6] = _in_struct->sonicDistanceL;
+  outBuf[7] = _in_struct->sonicDistanceR;
+  outBuf[8] = _in_struct->sonicDistanceB;
+  outBuf[9] = _in_struct->shoulder;
+  outBuf[10] = _in_struct->wrist;
+  outBuf[11] = _crc8(&outBuf[1], 10);
+  outBuf[12] = 0xdd;
 }
 
 Comm::~Comm() {
--- Robot/Config.h
+++ Robot/Config.h
@@ -8,6 +8,24 @@
 #define DRIVE_FR_PIN  9
 #define DRIVE_BR_PIN  10
 #define JUMP_PIN      32
+#define SONIC_T_F_PIN 1
+#define SONIC_E_F_PIN 1
+#define SONIC_T_L_PIN 1
+#define SONIC_E_L_PIN 1
+#define SONIC_T_R_PIN 1
+#define SONIC_E_R_PIN 1
+#define SONIC_T_B_PIN 1
+#define SONIC_E_B_PIN 1
+
+#define SHOULDER_MOTOR_PIN  1
+#define WRIST_MOTOR_PIN     1
+#define SHOULDER_POT_PIN    1
+#define WRIST_POT_PIN       1
+#define KEY_GRABBER_PIN     1
+
+#define INTAKE_PIN    1
+#define SCORE_PIN     1
+#define DOOR_PIN      1
 
 // Comms
 #define BAUD_RATE     57600
