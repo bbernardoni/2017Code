d0fa81b7ac960ad54a22a94785b292aa839fcc35
Hunter Huynh <Huntz256@gmail.com>
2017-03-04 14:45:22 -0600

    Add ball logic in Ball.cpp and Ball.h
    Add ball logic between ball intake / raise door / score button inputs
    and intake motor / solenoid / score motor outputs.

--- DriverStation/src/robot/Ball.cpp
+++ DriverStation/src/robot/Ball.cpp
@@ -1,19 +1,55 @@
 #include "Ball.h"
 
-Ball::Ball(){
-
+Ball::Ball() {
+	scoring = false;
+	scoreStartTime = 0;
+	intakeState = false;
+	solenoidState = false;
 }
 
-void Ball::periodic(const RobotIn& rIn, RobotOut& rOut){
-	// check for ball intake button (create in Controls.h)
-	// and toggle intake motor
+void Ball::periodic(const RobotIn& rIn, RobotOut& rOut) {
 
+	// If ball intake button is pressed (create in Controls.h), toggle intake motor
+	if (CTRL_INTAKE && !intakeState) {
+		intakeState = true;
+	}
+	else if (CTRL_INTAKE && intakeState) {
+		intakeState = false;
+	}
 
-	// check for raise door button and toggle solenoid
+	if (intakeState) {
+		rOut.intake = INTAKE_ON;
+	}
+	else {
+		rOut.intake = INTAKE_OFF;
+	}
 
+	// If raise door button is pressed, toggle solenoid
+	if (CTRL_DOOR && !solenoidState) {
+		solenoidState = true;
+	}
+	else if (CTRL_DOOR && solenoidState) {
+		solenoidState = false;
+	}
 
-	// check for score button and index servo motor
-	// see golf ball scorer from last years code 
-	// -- https://github.com/MRDC2019/iRobotics_Personal/blob/master/RobotMain/GolfBalls.cpp
+	if (solenoidState) {
+		rOut.solenoid = SOLENOID_ON;
+	}
+	else {
+		rOut.solenoid = SOLENOID_OFF;
+	}
+
+	// If score button, toggle score motor for some period of time
+	if (CTRL_SCORE) {
+		scoring = true;
+		scoreStartTime = time(0); // Get current time
+		rOut.score = SCORE_POSN;
+	}
+
+	if (scoring && (time(0) - scoreStartTime > HOLD_TIME)) {
+		scoring = false;
+		rOut.score = HOLD_POSN;
+	}
+
 
 }
--- DriverStation/src/robot/Ball.h
+++ DriverStation/src/robot/Ball.h
@@ -2,12 +2,18 @@
 
 #include "RobotIO.h"
 #include "Controls.h"
+#include "Constants.h"
+
+#include <ctime>
 
 class Ball {
 public:
 	Ball();
 	void periodic(const RobotIn& rIn, RobotOut& rOut);
-    
+
 private:
-	
+	bool scoring;
+	time_t scoreStartTime;
+	bool intakeState;
+	bool solenoidState;
 };
